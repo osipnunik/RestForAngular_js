0ab5e1b12bf73c31b8234c80136a228103d9dfbb
osipnunik <osipnunik@gmail.com>
2018-12-29 14:57:27 +0200

    first commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+class Employee {
+
+	private @Id @GeneratedValue Long id;
+	private String name;
+	private String role;
+
+	Employee(String name, String role) {
+		this.name = name;
+		this.role = role;
+	}
+	
+}
